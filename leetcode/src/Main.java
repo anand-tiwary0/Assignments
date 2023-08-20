ba39bf1c518eaa56ff31ff735424a09e1a66201c
Anonmityslayer <nonmityslayer@tutanota.com>
2023-08-21 01:21:39 +0530

    leetcode problem

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -593,3 +593,49 @@ class Solution {
         return ((n & (n-1)) == 0);
     }
 }
+
+
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/submissions/
+
+
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start<=end){
+            int mid = start + (end - start)/2;
+
+
+            if(target == arr[mid]){
+                return true;
+            } 
+            //in case all three are equal and we can't decide which part is sorted
+            if(arr[start] == arr[mid] && arr[end]==arr[mid])  //and this condition will hit only when target is not equal to any of them so just negelect them to reduce search space until we get exactly one sorted part to implement binary search logic
+            {
+                start = start + 1;
+                end = end -1;
+                continue;
+            }                       
+
+            if(arr[start]<=arr[mid]){
+                if(target>=arr[start] && target <= arr[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target<=arr[end] && target>=arr[mid]){
+                    start = mid+1 ;
+                }
+                else{
+                end = mid -1;
+                }
+            }
+        }
+        return false;
+    }
+}
