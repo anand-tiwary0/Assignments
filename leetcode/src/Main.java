a1bfb174f9a8c02c36ce115d0f0c20ab3d64baf4
Anonmityslayer <nonmityslayer@tutanota.com>
2023-08-26 22:23:32 +0530

    leetcode problem and comparable implements

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -740,3 +740,37 @@ class Solution {
 
     }
 }
+
+
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
+
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] arr = {-1,-1};
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+        arr[0] = start; arr[1] = end;
+        return arr;
+    }
+
+    public static int search(int[] nums,int target, boolean which){
+        int start = 0;
+        int end = nums.length - 1;
+        int ansindex = -1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]==target){
+                ansindex = mid;
+                //and then look for other possible answers
+                if(which == true) end = mid -1; //when we are looking for start index
+                else start = mid + 1; //we are now looking for end index;
+            }
+
+            else if(nums[mid]>target) end = mid -1;
+            else start = mid + 1;
+        }
+
+        return ansindex;
+    }
+}
